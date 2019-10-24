c0d5b76994a158a70273ff3c222877e74390848e
TheDankraiLorde <menezesgaurav@gmail.com>
2019-10-25 01:16:47 +0530

    Improved order redux component

--- src/components/menu_orders.jsx
+++ src/components/menu_orders.jsx
@@ -1,71 +1,97 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import { IonContent, IonList, IonItem, IonItemGroup, IonLabel, IonCheckbox, IonTitle, IonButton, IonInput } from '@ionic/react';
-import { withRouter } from 'react-router-dom';
-import { chooseItems } from '../redux/menu/menu.actions';
-import { addOrders } from '../redux/orders/orders.actions';
+import React from "react";
+import { connect } from "react-redux";
+import {
+  IonContent,
+  IonList,
+  IonItem,
+  IonItemGroup,
+  IonLabel,
+  IonCheckbox,
+  IonTitle,
+  IonButton,
+  IonInput
+} from "@ionic/react";
+import { withRouter } from "react-router-dom";
+import { chooseItems } from "../redux/menu/menu.actions";
+import { addOrders } from "../redux/orders/orders.actions";
 
-const MenuOrders = ({history, menu, orders, addOrders}) => {
-    return(
-        <IonContent>
-            <IonList>
-                <IonItem>
-                    <IonTitle>Menu</IonTitle>
-                </IonItem>
-                <IonItemGroup>
-                {
-                    menu.map((item) => (
-                        <IonItem key={item.id}>
-                            <IonLabel>{item.item}</IonLabel>
-                            <IonLabel>Rs {item.price}</IonLabel>
-                            <IonCheckbox checked={item.isSelect} onClick={() => {handleChange(item.id)}}/>
-                        </IonItem>
-                    ))
-                }   
-                </IonItemGroup>
-                <IonItem>
-                    <IonTitle>Ordered Items</IonTitle>
-                    <IonInput />
-                </IonItem>
-                <IonItemGroup>
-                {
-                    menu.filter(item=>(
-                        item.isSelect
-                    )).map((item) => (
-                        <IonItem key={item.id}>
-                            <IonLabel>{item.item}</IonLabel>
-                            <IonLabel>Rs {item.price}</IonLabel>
-                        </IonItem>
-                    ))
-                }   
-                <hr />
-                <IonTitle>Total: Rs {
-                    menu.filter(
-                        item=>(item.isSelect)
-                    ).reduce(
-                        (acc,item) => {
-                            return (acc+item.price)
-                        },0
-                    )
-                }</IonTitle>
-                <hr />
-                </IonItemGroup>
-            </IonList>
-            <IonButton onClick={() => {
-                handleSubmit();
-            }}>Submit</IonButton>
-        </IonContent>
-    )
-}
+const MenuOrders = ({ history, menu, selectItems, addOrders }) => {
+  const handleSubmit = () => {
+    const itemsOrdered = menu.filter(item => item.isSelect);
+    addOrders({
+      items: itemsOrdered,
+      status: "En Route"
+    });
+    history.goBack();
+  };
 
-const mstp = (state) => ({
-    menu: state.menu.menuList,
-    orders: state.order.orders
-})
+  return (
+    <IonContent>
+      <IonList>
+        <IonItem>
+          <IonTitle>Menu</IonTitle>
+        </IonItem>
+        <IonItemGroup>
+          {menu.map(item => (
+            <IonItem key={item.id}>
+              <IonLabel>{item.item}</IonLabel>
+              <IonLabel>Rs {item.price}</IonLabel>
+              <IonCheckbox
+                checked={item.isSelect}
+                onClick={() => {
+                  selectItems(item.id);
+                }}
+              />
+            </IonItem>
+          ))}
+        </IonItemGroup>
+        <IonItem>
+          <IonTitle>Ordered Items</IonTitle>
+          <IonInput />
+        </IonItem>
+        <IonItemGroup>
+          {menu
+            .filter(item => item.isSelect)
+            .map(item => (
+              <IonItem key={item.id}>
+                <IonLabel>{item.item}</IonLabel>
+                <IonLabel>Rs {item.price}</IonLabel>
+              </IonItem>
+            ))}
+          <hr />
+          <IonTitle>
+            Total: Rs{" "}
+            {menu
+              .filter(item => item.isSelect)
+              .reduce((acc, item) => {
+                return acc + item.price;
+              }, 0)}
+          </IonTitle>
+          <hr />
+        </IonItemGroup>
+      </IonList>
+      <IonButton
+        onClick={() => {
+          handleSubmit();
+        }}
+      >
+        Submit
+      </IonButton>
+    </IonContent>
+  );
+};
 
-const mdtp = (dispatch) =>({
-    selectItems: (item_id) => dispatch(chooseItems(item_id)),
-    addOrders: (order) => dispatch(addOrders(order))
+const mstp = state => ({
+  menu: state.menu.menuList,
+  orders: state.order.orders
 });
 
-export default connect(mstp,mdtp)(withRouter(MenuOrders));
+const mdtp = dispatch => ({
+  selectItems: item_id => dispatch(chooseItems(item_id)),
+  addOrders: order => dispatch(addOrders(order))
+});
+
+export default connect(
+  mstp,
+  mdtp
+)(withRouter(MenuOrders));
--- src/redux/orders/orders.reducer.js
+++ src/redux/orders/orders.reducer.js
@@ -1,18 +1,24 @@
-
 const INITIAL_STATE = {
-    orders: []
-}
+  orders: [],
+  orderId: 0
+};
 
-const orderReducer = (state = INITIAL_STATE,action) => {
-    switch(action.type){
-        case "ADD_ORDER":
-            return {
-                orders : [...state.orders, action.payload]
-            }
+const orderReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "ADD_ORDER":
+      return {
+        orders: [
+          ...state.orders,
+          {
+            ...action.payload,
+            orderId: state.orderId + 1
+          }
+        ]
+      };
 
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export default orderReducer;
